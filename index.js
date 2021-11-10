067d9cec19150e2e9f5cdf6f376a958b03a5afc3
Damon-Nunez <damon.nunez@student.uncommonhs.org>
2021-11-10 15:55:43 +0000

    Ok

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//write your code here
+function stripUpperCase(string) {
+let newString = ''
+
+for(let i=0; i< string.length;i++){
+    debugger
+
+let letter=string[i]
+
+if(letter.toLowerCase()=== letter){
+    newString+= letter
+}
+}
+return newString
+}
